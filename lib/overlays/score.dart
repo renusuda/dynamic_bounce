5d375e9704a31f09894d9ee0ab640ff14552accf
Ru2Lu <jiutianlian0@gmail.com>
2025-02-11 00:16:34 +0900

    Display score on the score overlay

--- lib/overlays/score.dart
+++ lib/overlays/score.dart
@@ -1,18 +1,21 @@
+import 'package:dynamic_bounce/providers/score.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// The score overlay.
-class Score extends StatelessWidget {
+class Score extends ConsumerWidget {
   /// Creates a new score overlay.
   const Score({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
-    return const Center(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final score = ref.watch(scoreProvider);
+    return Center(
       child: Text(
-        'SCORE',
-        style: TextStyle(
+        'SCORE: $score',
+        style: const TextStyle(
           fontWeight: FontWeight.bold,
         ),
       ),
