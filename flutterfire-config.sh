ecc2f55dd4f331e018d6533bae6dfefa0fe7abe2
Ru2Lu <jiutianlian0@gmail.com>
2025-03-09 12:34:42 +0900

    Configure firebase for android

--- flutterfire-config.sh
+++ flutterfire-config.sh
@@ -13,14 +13,18 @@ case $1 in
       --project=dynamic-bounce-dev \
       --out=lib/firebase_options_dev.dart \
       --ios-bundle-id=com.renusuda.dynamicBounce.dev \
-      --ios-out=ios/flavors/dev/GoogleService-Info.plist
+      --ios-out=ios/flavors/dev/GoogleService-Info.plist \
+      --android-package-name=com.renusuda.dynamic_bounce.dev \
+      --android-out=android/app/src/dev/google-services.json
     ;;
   prod)
     flutterfire config \
       --project=dynamic-bounce-prod \
       --out=lib/firebase_options_prod.dart \
       --ios-bundle-id=com.renusuda.dynamicBounce \
-      --ios-out=ios/flavors/prod/GoogleService-Info.plist
+      --ios-out=ios/flavors/prod/GoogleService-Info.plist \
+      --android-package-name=com.renusuda.dynamic_bounce.prod \
+      --android-out=android/app/src/prod/google-services.json
     ;;
   *)
     echo "Error: Invalid environment specified. Use 'dev' or 'prod'."
